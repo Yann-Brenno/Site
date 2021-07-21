a7ebfb7413666dd0ca00bb55e5ba417629acbf61
Misifus <44626447+misifus-wowpro@users.noreply.github.com>
2021-07-21 13:49:53 -0300

    Atualização de fundo

---
+++ site.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, inicial-sacale=1.0">
+<title>CADASTRO</title>
+<link rel="stylesheet" href="css/links.css">
+<link rel="icon" href="imagens/fav icon.png">
+</head>
+<body>
+<div id="list-container">
+<h1>CADASTRO</h1>
+<form name="cadastrar" method="post" action="cadastro.php">
+<ul> Email: </ul>
+<ul><input type="text" name="email" /> </ul><br></br>
+<ul>Confirmação de de email: </ul>
+<ul> <input type="text" name="confirmacao"/></ul><br> </br>
+<ul>Senha: </ul>
+<ul><input type="password" name="senha"/></ul><br> </br>
+<ul>Confirmação de senha: </ul>
+<ul> <input type="password" name="confirmarsenha"/></ul><br> </br>
+<input type="submit" value="cadastrar"/>
+</div>
+<footer>
+Página desenvolvida por YANN BRENNO DA COSTA RAMOS   © 2021
+</footer>
+
+
+</body>
+</html>
